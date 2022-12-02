3a49491bb725ac1d3c00d01b74da9195a1d000cc
madosuki <yurikasamaaikatsu@gmail.com>
2022-12-03 01:19:22 +0900

    separate function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
-fn main() {
-    let devices = rusb::devices();
+use rusb::{
+    UsbContext,
+    DeviceList,
+    GlobalContext,
+};
+
+fn show_device(device_context: DeviceList<GlobalContext>) {    
     
-    for device in devices.unwrap().iter() {
+    for device in device_context.iter() {
         let device_desc = device.device_descriptor().unwrap();
 
         println!("Bus {:03}", device.bus_number());
+    }
+}
+
+fn main() {
+    let devices = rusb::devices();
+    
+    match devices {
+        Ok(_v) => {
+            println!("found!");
+            show_device(_v);
+        },
+        Err(_e) => panic!("{:?}", _e),
     }
 }
